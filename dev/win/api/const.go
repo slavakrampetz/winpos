37f26f19b5390036c354b562003f85aff24ca4d8
Slava Krampetz <svl@dev4masses.com>
2022-04-16 15:46:10 +0700

    constants: reformat

--- dev/win/api/const.go
+++ dev/win/api/const.go
@@ -1,59 +1,60 @@
+//go:build windows && amd64
 // +build windows,amd64
 
 package winapi
 
 //goland:noinspection GoSnakeCaseUsage,GoUnusedConst,SpellCheckingInspection
 const (
-	ERROR_SUCCESS   = 0
+	ERROR_SUCCESS = 0
 )
 
 // GetWindowLong and GetWindowLongPtr constants
 //goland:noinspection GoSnakeCaseUsage,GoUnusedConst,SpellCheckingInspection
 const (
-	GWL_EXSTYLE     = -20
-	GWL_STYLE       = -16
-	GWL_WNDPROC     = -4
-	GWL_HINSTANCE   = -6
-	GWL_HWNDPARENT  = -8
-	GWL_ID          = -12
-	GWL_USERDATA    = -21
+	GWL_EXSTYLE    = -20
+	GWL_STYLE      = -16
+	GWL_WNDPROC    = -4
+	GWL_HINSTANCE  = -6
+	GWL_HWNDPARENT = -8
+	GWL_ID         = -12
+	GWL_USERDATA   = -21
 )
 
 // Window style constants
 //goland:noinspection GoSnakeCaseUsage,GoUnusedConst,SpellCheckingInspection
 const (
-	WS_OVERLAPPED       = 0X00000000
-	WS_POPUP            = 0X80000000
-	WS_CHILD            = 0X40000000
-	WS_MINIMIZE         = 0X20000000
-	WS_VISIBLE          = 0X10000000
-	WS_DISABLED         = 0X08000000
-	WS_CLIPSIBLINGS     = 0X04000000
-	WS_CLIPCHILDREN     = 0X02000000
-	WS_MAXIMIZE         = 0X01000000
-	WS_CAPTION          = 0X00C00000
-	WS_BORDER           = 0X00800000
-	WS_DLGFRAME         = 0X00400000
-	WS_VSCROLL          = 0X00200000
-	WS_HSCROLL          = 0X00100000
-	WS_SYSMENU          = 0X00080000
-	WS_THICKFRAME       = 0X00040000
-	WS_GROUP            = 0X00020000
-	WS_TABSTOP          = 0X00010000
-	WS_MINIMIZEBOX      = 0X00020000
-	WS_MAXIMIZEBOX      = 0X00010000
-	WS_TILED            = 0X00000000
-	WS_ICONIC           = 0X20000000
-	WS_SIZEBOX          = 0X00040000
-	WS_OVERLAPPEDWINDOW = 0X00000000 | 0X00C00000 | 0X00080000 | 0X00040000 | 0X00020000 | 0X00010000
-	WS_POPUPWINDOW      = 0X80000000 | 0X00800000 | 0X00080000
-	WS_CHILDWINDOW      = 0X40000000
+	WS_OVERLAPPED       = 0x00000000
+	WS_POPUP            = 0x80000000
+	WS_CHILD            = 0x40000000
+	WS_MINIMIZE         = 0x20000000
+	WS_VISIBLE          = 0x10000000
+	WS_DISABLED         = 0x08000000
+	WS_CLIPSIBLINGS     = 0x04000000
+	WS_CLIPCHILDREN     = 0x02000000
+	WS_MAXIMIZE         = 0x01000000
+	WS_CAPTION          = 0x00C00000
+	WS_BORDER           = 0x00800000
+	WS_DLGFRAME         = 0x00400000
+	WS_VSCROLL          = 0x00200000
+	WS_HSCROLL          = 0x00100000
+	WS_SYSMENU          = 0x00080000
+	WS_THICKFRAME       = 0x00040000
+	WS_GROUP            = 0x00020000
+	WS_TABSTOP          = 0x00010000
+	WS_MINIMIZEBOX      = 0x00020000
+	WS_MAXIMIZEBOX      = 0x00010000
+	WS_TILED            = 0x00000000
+	WS_ICONIC           = 0x20000000
+	WS_SIZEBOX          = 0x00040000
+	WS_OVERLAPPEDWINDOW = 0x00000000 | 0x00C00000 | 0x00080000 | 0x00040000 | 0x00020000 | 0x00010000
+	WS_POPUPWINDOW      = 0x80000000 | 0x00800000 | 0x00080000
+	WS_CHILDWINDOW      = 0x40000000
 )
 
 // https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-systemparametersinfoa
 //goland:noinspection GoUnusedConst,GoSnakeCaseUsage,SpellCheckingInspection
 const (
-	SPI_GETSCREENSAVEACTIVE     uint32 = 0x0010
-	SPI_GETSCREENSAVERRUNNING   uint32 = 0x0072
-	SPI_GETSCREENSAVESECURE     uint32 = 0x0076
+	SPI_GETSCREENSAVEACTIVE   uint32 = 0x0010
+	SPI_GETSCREENSAVERRUNNING uint32 = 0x0072
+	SPI_GETSCREENSAVESECURE   uint32 = 0x0076
 )
